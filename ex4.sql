9d3bc6abdbfdcbe64bb47bfeb1678992a365c064
Emeka Mbazor <47117694+esm2000@users.noreply.github.com>
2020-03-10 01:56:09 -0400

    insert referential data to connect person and pet tables

---
+++ ex4.sql
@@ -0,0 +1,8 @@
+INSERT INTO person_pet(person_id, pet_id) VALUES (0,0);
+INSERT INTO person_pet VALUES (0,1);
+
+INSERT INTO person(id, first_name, last_name, age)
+    VALUES (1, "Emeka", "Mbazor", 21);
+
+INSERT INTO person_pet(person_id, pet_id) VALUES (1,0);
+INSERT INTO person_pet VALUES (1,1);
