0a67c4ae33ee7965486e375c5b6bf906445cf90e
Emeka Mbazor <47117694+esm2000@users.noreply.github.com>
2020-03-10 20:58:59 -0400

    select data from database using SELECT clause in combination with conditions

---
+++ ex5.sql
@@ -0,0 +1,15 @@
+SELECT * FROM person;
+
+SELECT name,age FROM pet;
+
+SELECT name,age FROM pet WHERE dead = 0;
+
+SELECT * FROM person WHERE first_name = "Zed";
+
+SELECT * FROM pet WHERE age > 10;
+
+SELECT * FROM person WHERE age < 21;
+
+SELECT * FROM person WHERE age > 21;
+
+SELECT * FROM person WHERE first_name = "Emeka" AND age = 21;
